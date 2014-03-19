1616630266a2ee7cb3b8836f6c639b2175a8c0ad
Giancarlo Silva <giancarlozero@gmail.com>
2014-03-18 23:30:07 -0300

    Tentando corrigir o erro do formulário para que ele seja exibido ao clicar no link "Adicionar novo item" na home.

--- agenda/forms.py
+++ agenda/forms.py
@@ -4,10 +4,10 @@ from django import forms
 from models import ItemAgenda
 
 class FormItemAgenda(forms.Form):
-	titulo = forms.CharField(max_length = 100)
 	data = forms.DateField(
 		widget = forms.DateInput(format='%d/%m/%Y'),
 		input_formats=['%d/%m/%y', '%d/%m/%Y']
 	)
 	hora = forms.TimeField()
+	titulo = forms.CharField(max_length = 100)
 	descricao = forms.CharField()
