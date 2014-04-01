0b48457c09f0a44a3e0c1d5c7c1821fd3f4c0d2c
Giancarlo Silva <giancarlozero@gmail.com>
2014-03-31 22:35:56 -0300

    UTF-8 adicionado. Comentários adicionados.

--- crud/urls.py
+++ crud/urls.py
@@ -1,3 +1,5 @@
+# -*- coding: utf8 -*-
+
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
@@ -8,8 +10,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'crud.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'agenda.views.lista'),
-    url(r'^adiciona/$', 'agenda.views.adiciona', name='adiciona'),
-    url(r'^item/(?P<nr_item>\d+)/$', 'agenda.views.item'),
+    url(r'^admin/', include(admin.site.urls)), # Para a criação de painel de administração. Não será usado neste exemplo
+    url(r'^$', 'agenda.views.lista'), # Exibe os itens listados
+    url(r'^adiciona/$', 'agenda.views.adiciona', name='adiciona'), # Formulário do template "adiciona.html"
+    url(r'^item/(?P<nr_item>\d+)/$', 'agenda.views.item'), # URL individual para cada item cadastrado
 )
